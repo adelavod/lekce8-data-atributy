d0fb537ea04de7398ed95453d54cc67ee55f1c68
Adéla Krásová <>
2022-04-06 16:12:46 +0200

    [FEATURE] fruit calories count added

--- main.js
+++ main.js
@@ -9,3 +9,28 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+
+const obrazky = document.querySelectorAll(".obrazek");
+let soucetKalorii = 0;
+let seznam = document.getElementById("tretinka");
+
+
+obrazky.forEach((obrazek) => {
+    obrazek.addEventListener("click", vypisKalorie); // forEach připojuje funkci pro každý obrázel v poli "obrazky"
+    
+});
+
+function vypisKalorie(udalost) { //tohle je ta funkce
+    let obrazek = udalost.target; //UDALOST.TARGET !!!
+    let kalorie = parseInt(obrazek.dataset.kalorie);
+    soucetKalorii = soucetKalorii + kalorie;
+    document.querySelector("#vysledek").textContent = kalorie;
+
+    let totalKalorie = document.querySelector("#totalKalorie");
+    totalKalorie.textContent = soucetKalorii;
+
+    obrazek.removeEventListener("click", vypisKalorie); // nakonec odebereme funkci EventListeneru
+};
+
+
